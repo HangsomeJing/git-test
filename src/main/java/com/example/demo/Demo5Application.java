19959a5dc8cd1763c2fc6d4710c252c4bc4c84d9
jpl <jpl@test.com>
2022-11-11 21:58:43 +0800

    "first comiit"

--- src/main/java/com/example/demo/Demo5Application.java
+++ src/main/java/com/example/demo/Demo5Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo5Application {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SpringApplication.run(Demo5Application.class, args);
     }
 
