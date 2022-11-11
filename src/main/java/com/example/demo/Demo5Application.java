3c2c5531edd65fbe2c05fba82eaf31f841f2e9ff
jpl <jpl@test.com>
2022-11-11 21:59:31 +0800

    "third comiit"

--- src/main/java/com/example/demo/Demo5Application.java
+++ src/main/java/com/example/demo/Demo5Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo5Application {
 
     public static void main(String[] args) {
-        System.out.println("test");
+        System.out.println("test2");
         SpringApplication.run(Demo5Application.class, args);
     }
 
